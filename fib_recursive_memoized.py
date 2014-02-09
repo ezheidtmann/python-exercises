fbd3e97e3fd930addba1eca48a5fe6434fc4307c
Evan Heidtmann <evan.heidtmann@gmail.com>
2014-02-08 17:38:33 -0800

    Add good tracing for memoized version

--- fib_recursive_memoized.py
+++ fib_recursive_memoized.py
@@ -1,11 +1,12 @@
 def fib(n):
-    print 'fib(%d)' % n
     if (not hasattr(fib, 'known')):
-        fib.known = [1, 1, 1]
+        fib.known = [0, 1, 1]
         print "making list"
 
     if (len(fib.known) <= n or not fib.known[n]):
-        print "computing %d" % n
+        print "%d: %s" % (n, str(fib.known))
+        print "%d: computing %d" % (n, n)
         fib.known.insert(n, fib(n-2) + fib(n-1))
+        print "%d: %s" % (n, str(fib.known))
 
     return fib.known[n]
